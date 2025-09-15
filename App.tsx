29634df01846c8b86096f507850d28269651e35e
mohamedshawky982 <mohamedshawkym90@gmail.com>
2025-09-15 15:49:45 +0300

    remove imports

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import {Text} from 'react-native';
 import {Provider} from 'react-redux';
 import store, {persistedStore} from './src/state-management';
 import {PersistGate} from 'redux-persist/integration/react';
